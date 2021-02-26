142b6a08bb90c7bcbb5f809c590652dd45786e7d
sealfury <willspecker@gmail.com>
2021-02-26 16:21:42 +0100

    refactors reader and summary with static methods to clean up index

--- build/index.js
+++ build/index.js
@@ -1,13 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
-var CsvFileReader_1 = require("./CsvFileReader");
-var WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
 var SummaryAnalysis_1 = require("./SummaryAnalysis");
-var HtmlReport_1 = require("./reportTargets/HtmlReport");
 // NB 'match' refers to football match
-var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
-var matchReader = new MatchReader_1.MatchReader(csvFileReader);
+var matchReader = MatchReader_1.MatchReader.fromCsv('football.csv');
+var summaryChelsea = SummaryAnalysis_1.SummaryAnalysis.winsWithHtml('Chelsea');
 matchReader.load();
-var summaryChelsea = new SummaryAnalysis_1.SummaryAnalysis(new WinsAnalysis_1.WinsAnalysis('Chelsea'), new HtmlReport_1.HtmlReport());
 summaryChelsea.buildAndPrintReport(matchReader.matches);
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,10 @@
 import { MatchReader } from './MatchReader'
-import { CsvFileReader } from './CsvFileReader'
-import { ConsoleReport } from './reportTargets/ConsoleReport'
-import { WinsAnalysis } from './analyzers/WinsAnalysis'
 import { SummaryAnalysis } from './SummaryAnalysis'
-import { HtmlReport } from './reportTargets/HtmlReport'
 
 // NB 'match' refers to football match
 
-const csvFileReader = new CsvFileReader('football.csv')
+const matchReader = MatchReader.fromCsv('football.csv')
+const summaryChelsea = SummaryAnalysis.winsWithHtml('Chelsea')
 
-const matchReader = new MatchReader(csvFileReader)
 matchReader.load()
-
-const summaryChelsea = new SummaryAnalysis(
-  new WinsAnalysis('Chelsea'),
-  new HtmlReport()
-)
-
 summaryChelsea.buildAndPrintReport(matchReader.matches)
