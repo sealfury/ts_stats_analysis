7acd8a2c93023b04d31a272ac0c203b9dc85ea11
sealfury <willspecker@gmail.com>
2021-02-26 16:26:24 +0100

    formats js

--- build/SummaryAnalysis.js
+++ build/SummaryAnalysis.js
@@ -1,20 +1,23 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SummaryAnalysis = void 0;
-var WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
-var HtmlReport_1 = require("./reportTargets/HtmlReport");
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
+exports.SummaryAnalysis = void 0
+var WinsAnalysis_1 = require('./analyzers/WinsAnalysis')
+var HtmlReport_1 = require('./reportTargets/HtmlReport')
 var SummaryAnalysis = /** @class */ (function () {
-    function SummaryAnalysis(analyzer, outputTarget) {
-        this.analyzer = analyzer;
-        this.outputTarget = outputTarget;
-    }
-    SummaryAnalysis.winsWithHtml = function (team) {
-        return new SummaryAnalysis(new WinsAnalysis_1.WinsAnalysis(team), new HtmlReport_1.HtmlReport());
-    };
-    SummaryAnalysis.prototype.buildAndPrintReport = function (matches) {
-        var output = this.analyzer.run(matches);
-        this.outputTarget.print(output);
-    };
-    return SummaryAnalysis;
-}());
-exports.SummaryAnalysis = SummaryAnalysis;
+  function SummaryAnalysis(analyzer, outputTarget) {
+    this.analyzer = analyzer
+    this.outputTarget = outputTarget
+  }
+  SummaryAnalysis.winsWithHtml = function (team) {
+    return new SummaryAnalysis(
+      new WinsAnalysis_1.WinsAnalysis(team),
+      new HtmlReport_1.HtmlReport()
+    )
+  }
+  SummaryAnalysis.prototype.buildAndPrintReport = function (matches) {
+    var output = this.analyzer.run(matches)
+    this.outputTarget.print(output)
+  }
+  return SummaryAnalysis
+})()
+exports.SummaryAnalysis = SummaryAnalysis
