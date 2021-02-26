510ea91d77b56b87ca236f502a0824dddaed1ce0
sealfury <willspecker@gmail.com>
2021-02-26 16:32:58 +0100

    adds build folder to gitignore

--- build/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-var MatchReader_1 = require('./MatchReader')
-var SummaryAnalysis_1 = require('./SummaryAnalysis')
-// NB 'match' refers to football match
-var matchReader = MatchReader_1.MatchReader.fromCsv('football.csv')
-var summaryChelsea = SummaryAnalysis_1.SummaryAnalysis.winsWithHtml('Chelsea')
-matchReader.load()
-summaryChelsea.buildAndPrintReport(matchReader.matches)
