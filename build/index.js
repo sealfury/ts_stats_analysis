dc44fe04e46e6ee44ebb0d6644196b1df3f63bc8
sealfury <willspecker@gmail.com>
2021-02-26 13:05:08 +0100

    creates functionality to analyse one element of data

--- build/index.js
+++ build/index.js
@@ -1,15 +1,28 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = __importDefault(require("fs"));
+'use strict'
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod }
+  }
+Object.defineProperty(exports, '__esModule', { value: true })
+var fs_1 = __importDefault(require('fs'))
+// NB variable 'matches' refers to *football* matches,
+// NOT matches as in 'equivalents'
 var matches = fs_1.default
-    .readFileSync('football.csv', {
+  .readFileSync('football.csv', {
     encoding: 'utf-8',
-})
-    .split('\n')
-    .map(function (row) {
-    return row.split(',');
-});
-console.log(matches);
+  })
+  .split('\n')
+  .map(function (row) {
+    return row.split(',')
+  })
+var manUWins = 0
+for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+  var match = matches_1[_i]
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUWins++
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUWins++
+  }
+}
+console.log('ManU won ' + manUWins + ' games')
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import fs from 'fs'
 
+// NB variable 'matches' refers to *football* matches,
+// NOT matches as in 'equivalents'
+
 const matches = fs
   .readFileSync('football.csv', {
     encoding: 'utf-8',
@@ -9,4 +12,14 @@ const matches = fs
     return row.split(',')
   })
 
-console.log(matches)
+let manUWins = 0
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUWins++
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUWins++
+  }
+}
+
+console.log(`ManU won ${manUWins} games`)
